8ab28d890ad4004af1bd34010d1883644748fd10
Jeffrey Yang <yjeffrey@seas.upenn.edu>
2023-11-23 15:03:02 -0500

    example in main

--- src/main.rs
+++ src/main.rs
@@ -10,41 +10,47 @@ mod types;
 
 fn main() {
     let db = database::get_global_db();
+
+    // 1. Creates a table called employee with two columns: id (int) and name (string)
     db.get_catalog().load_schema();
+
+    // 2. Retrieve the list of catalogs
     let catalog = db.get_catalog();
+
+    // 3. Retrieve the table id for the employee table
     let table = catalog.get_table_from_name("employee").unwrap();
     let table_id = table.get_id();
+
+    // 4. Retrieve the tuple descriptor for the employee table
     let td = table.get_tuple_desc().clone();
 
     print!("table id: {}\n", table_id);
     print!("table name: {:?}\n", td.get_field_name(0));
 
-    // let bp = db.get_buffer_pool();
-    // bp.insert_tuple(
-    //     table_id,
-    //     tuple::Tuple::new(
-    //         vec![
-    //             fields::FieldVal::IntField(fields::IntField::new(1)),
-    //             fields::FieldVal::StringField(fields::StringField::new("Alice".to_string(), 5)),
-    //         ],
-    //         &td,
-    //     ),
-    // );
+    // 5. Insert two tuples into the employee table
+    let bp = db.get_buffer_pool();
+    bp.insert_tuple(
+        table_id,
+        tuple::Tuple::new(
+            vec![
+                fields::FieldVal::IntField(fields::IntField::new(1)),
+                fields::FieldVal::StringField(fields::StringField::new("Alice".to_string(), 5)),
+            ],
+            &td,
+        ),
+    );
+    bp.insert_tuple(
+        table_id,
+        tuple::Tuple::new(
+            vec![
+                fields::FieldVal::IntField(fields::IntField::new(2)),
+                fields::FieldVal::StringField(fields::StringField::new("Bob".to_string(), 3)),
+            ],
+            &td,
+        ),
+    );
 
-    // bp.insert_tuple(
-    //     table_id,
-    //     tuple::Tuple::new(
-    //         vec![
-    //             fields::FieldVal::IntField(fields::IntField::new(2)),
-    //             fields::FieldVal::StringField(fields::StringField::new("Bob".to_string(), 3)),
-    //         ],
-    //         &td,
-    //     ),
-    // );
-
-    // let pid = heap_page::HeapPageId::new(table_id, 0);
-    // let page = bp.get_page(pid).unwrap();
-    // let page_read = page.read().unwrap();
+    // 6. Print out the tuples in the employee table
     let table = catalog.get_table_from_id(table_id).unwrap();
     for page in table.iter() {
         for tuple in page.iter() {
